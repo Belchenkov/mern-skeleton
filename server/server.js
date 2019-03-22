5b2b603e7f1871cfcba963238239aa2af8642223
Aleksey Belchenkov <u608110@gmail.com>
2019-03-22 19:34:46 +0300

    Connect To MongoDB

--- server/server.js
+++ server/server.js
@@ -2,9 +2,21 @@ import config from './../config/config';
 import app from './express';
 import mongoose from 'mongoose';
 
-app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.info('Server started on port %s.', config.port);
+// Connection URL
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri,  { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB connecting');
+        app.listen(config.port, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.info('Server started on port %s.', config.port);
+        });
+    })
+    .catch(err => console.log(err));
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${mongoUri}`)
 });
+
+
