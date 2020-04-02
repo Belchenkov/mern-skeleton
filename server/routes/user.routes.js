cd75eac83ad4a55f093c3b82226ff0c176b24b62
Aleksey Belchenkov <u608110@gmail.com>
2020-04-02 19:08:17 +0300

    User controller

---
+++ server/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+const router = express.Router();
+
+import userCtrl from '../controllers/user.controller';
+
+router.route('/api/users')
+    .get(userCtrl.list)
+    .post(userCtrl.create);
+
+router.route('/api/users/:userId')
+    .get(userCtrl.read)
+    .put(userCtrl.update)
+    .delete(userCtrl.remove);
+
+router.param('userId', userCtrl.userByID);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import config from './../config/config';
 import app from './express';
 import mongoose from 'mongoose';
 
+import userRoutes from './routes/user.routes'
+
 // Connection URL
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUri,  { useNewUrlParser: true })
@@ -11,7 +13,11 @@ mongoose.connect(config.mongoUri,  { useNewUrlParser: true })
             if (err) {
                 console.log(err);
             }
+
             console.info('Server started on port %s.', config.port);
+
+            app.use('/', userRoutes)
+
         });
     })
     .catch(err => console.log(err));
