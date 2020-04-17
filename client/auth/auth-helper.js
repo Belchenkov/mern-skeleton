78431a515130fa5cba35f8a08f600c4e7958c251
Aleksey Belchenkov <u608110@gmail.com>
2020-04-17 20:04:35 +0300

    Delete Profile

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -24,9 +24,9 @@ const auth = {
         cb();
 
         //optional
-        signout().then((data) => {
+        signout().then(data => {
             document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        })
+        });
     }
 };
 
