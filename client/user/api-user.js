b6b879b711007f7ecf54d9f72ba9c93083e8396d
Aleksey Belchenkov <u608110@gmail.com>
2020-04-23 18:12:03 +0300

    Update api-user

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,67 +1,79 @@
-const create = user => {
-    return fetch('/api/users/', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => {
-        return response.json();
-    }).catch(err => console.log(err));
-};
-
-const list = () => {
-    return fetch('/api/users/', {
-        method: 'GET',
-    }).then(response => {
-        return response.json();
-    }).catch(err => console.log(err));
-};
-
-const read = (params, {token}) => {
-    return fetch(`/api/users/${params.userId}`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    }).then((response) => {
-        return response.json();
-    }).catch((err) => console.log(err))
-}
-
-const update = (params, { token }, user) => {
-    return fetch(`/api/users/${params.userId}`, {
-        method: 'PUT',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        body: JSON.stringify(user)
-    }).then(response => {
-        return response.json();
-    }).catch(err => {
+const create = async user => {
+    try {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        return await response.json();
+    } catch(err) {
         console.log(err);
-    });
+    }
 };
 
-const remove = (params, {token}) => {
-    return fetch(`/api/users/${params.userId}`, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    }).then(response => {
-        return response.json();
-    }).catch(err => {
-        console.log(err);
-    });
+const list = async signal => {
+    try {
+        const response = await fetch('/api/users/', {
+            method: 'GET',
+            signal: signal,
+        })
+        return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
+};
+
+const read = async (params, credentials, signal) => {
+    try {
+        const response = await fetch('/api/users/' + params.userId, {
+            method: 'GET',
+            signal: signal,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        })
+        return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
+};
+
+const update = async (params, credentials, user) => {
+    try {
+        const response = await fetch('/api/users/' + params.userId, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            },
+            body: JSON.stringify(user)
+        })
+        return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
+};
+
+const remove = async (params, credentials) => {
+    try {
+        const response = await fetch('/api/users/' + params.userId, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        })
+        return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
 };
 
 export { create, list, read, update, remove };
